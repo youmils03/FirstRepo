b061566949ae3df6bd411f53a6fb05e075d73a28
Alexander Stoddard <alexander.stoddard764@gmail.com>
2022-10-22 04:45:51 -0700

    shit I accidentally said git add -AA instead of git add -A but then when I said git add -A it didn't do anything so hopefully it's chill

---
+++ PycharmProjects/pythonProject/sigep.py
@@ -0,0 +1,5 @@
+a = "apple" , "arthur" , "aardvark" 
+print( type(a) )
+print( list(a) )
+print( a == tuple(a) )
+print( a == list(a) ) 
---
+++ lesson2.py
@@ -0,0 +1,12 @@
+print( 5 + 1 )  # addition
+print( 6 - 10 )  # subtraction
+print( 8 * 3 )  # multiplication
+print( 10 / 2 )  # division
+print( 9 / 2 )
+print( 9 // 2 )  # floor division, integer-based division like Java
+print( 7 % 2 )  # modular, like remainder
+print( 10 ** 3 )  # exponential
+print( 2.0 ** 3 )  # exponential with a floating point number
+print( 8 ** 2.0 )
+print( 8 ** 0.5 )
+print( 3 + 5 * 4 + 2 * 10 )  # order of operations
