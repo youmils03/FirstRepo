9d432a7c63f39ddb862e681292c8b91e5af7bece
Alexander Stoddard <astoddardmrm@gmail.com>
2022-10-31 15:41:18 -0700

    Update lesson70.py
    pass by value class

--- Python/YoutubeLessons/lesson70.py
+++ Python/YoutubeLessons/lesson70.py
@@ -1,3 +1,30 @@
+class PBV:
+  
+    @classmethod
+    def tryUpdating(c,x):
+      """Class method that shows that x comes in by value, not by reference
+      @param x the variable to try concatenating a string onto"""
+      x += " is awesome"
+      
+    def __init__(self,val):
+      self.val = val
+      
+    def update1(self):
+      """Solution 1 to the pass by value phenomenon: Use an instance variable"""
+      self.val = self.val + " is awesome"
+      
+    @classmethod
+    def update2(c,x):
+      """Solution 2 to the pass by value phenomenon: Use a direct return statement
+      @param x the variable to concatenate a string onto
+      @return the variable with the string concatenated on"""
+      return x + " is awesome"
+    
+    @staticmethod
+    def update3(x):
+      """Solution 3 to the pass by value phenomenon: Use a list with at least one element"""
+      x[0] += " is awesome"
+
 print( chr(84) )
 print( chr(85) )
 print( chr(86) )
@@ -23,3 +50,21 @@ for k in range(3):
 assert list(enumerate([4,4,2]) ) == list( zip( list( range( len( [4,4,2] ) )), [4,4,2]))  # throws an error if False
 
 print( "yup, enumerate is just zip(list(range(len(x))),x)" )
+
+# Pass by value
+
+PBV.tryUpdating(t := "Jake")
+print(t)
+
+u = PBV("Stephanie")
+u.update1( )
+print( u.val )
+
+v = "Blanca"
+print( PBV.update2(v) )
+
+w = [ "Teddy" ]
+PBV.update3(w)
+
+for x in w:
+  print(x)
