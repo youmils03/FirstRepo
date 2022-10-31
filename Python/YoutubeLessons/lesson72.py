c5b4740f2d2b36d81e908d88320db378e925af27
Alexander Stoddard <astoddardmrm@gmail.com>
2022-10-31 16:06:50 -0700

    Create lesson72.py

---
+++ Python/YoutubeLessons/lesson72.py
@@ -0,0 +1,28 @@
+set1 = { 4 , 0 , "bucket" , "tennis" }
+set2 = frozenset( {0,4,0} )
+
+print( set1.difference(set2) )
+print( set1 - set2 )
+
+print( set1.issuperset(set2) )
+print( set2.issubset(set1) )
+print( set1 | set2 ) # union
+print( set2 & set1 ) # intersection
+
+# Recall from the previous lesson that keys in a dictionary cannot be lists or sets. They must be immutable (tuples or frozen sets)
+
+colle = ( 9 , 19 , 12 , 10 , "soup" , "eggs" , "lettuce" , False )
+
+print( n := iter(colle) )
+
+j = range(len(colle))
+
+for k in j:
+  try:
+    print(next(n))
+  except StopIteration:
+    print( "no next element" )
+  else:
+    print( "never reached stop iteration since the for loop terminated when it should have" )
+  finally:
+    print( "fun stuff!" )
