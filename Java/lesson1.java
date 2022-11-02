ce750761a77bc015b4f8bc2dac1bfc188996ad92
Alexander Stoddard <astoddardmrm@gmail.com>
2022-11-02 10:36:14 -0700

    Create lesson1.java

---
+++ Java/lesson1.java
@@ -0,0 +1,18 @@
+public class Lesson1
+{
+    public static void main( String args [ ] )
+    {
+        System.out.println( "Summer is my favorite season" ); //this prints a statement and then a new line
+        System.out.println( "Spring is also a nice season" ); //this prints a statement and then a new line
+      
+        System.out.print( "just " ); //prints a statement without printing a new line afterwards
+        System.out.print( "a " );
+        System.out.print( "single " );
+        System.out.println( "word" );
+      
+        /*
+         * multiple-line comment
+         * used for longer explanations
+         */
+    }
+}
