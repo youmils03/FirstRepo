b418e845d7ee8e74c34dcb05d836d84a53d5ecb5
Alexander Stoddard <astoddardmrm@gmail.com>
2023-01-16 04:36:09 -0700

    Create lesson1.cs
    learn to print a statement and produce comments

---
+++ lesson1.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MySpace
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            
+            Console.WriteLine( "Peter is 25" ); //this prints a statement and then ends the current line
+            Console.Write( "Abby " ); //this prints a statement but remains on the current line
+            Console.WriteLine( "is 72" ); //continues the statement on the same line as Abby, then ends that line
+            
+            /*
+            This is
+            how you perform
+            a multiple line comment
+            Comment your code!
+            */
+        }
+    }
+}
